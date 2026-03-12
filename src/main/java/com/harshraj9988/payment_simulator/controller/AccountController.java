9d70e8af24bd6ddc9cc74bb2ffe34629656ed2ca
Harsh Raj <harshraj9988@gmail.com>
2026-03-12 23:17:00 +0530

    add account fetch and deposit REST API

--- src/main/java/com/harshraj9988/payment_simulator/controller/AccountController.java
+++ src/main/java/com/harshraj9988/payment_simulator/controller/AccountController.java
@@ -1,14 +1,15 @@
 package com.harshraj9988.payment_simulator.controller;
 
 import com.harshraj9988.payment_simulator.dto.AccountDto;
+import com.harshraj9988.payment_simulator.entity.Account;
 import com.harshraj9988.payment_simulator.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -24,6 +25,22 @@ public class AccountController {
     // Add account REST API
     @PostMapping
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
-       return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+       AccountDto addedAccount = accountService.createAccount(accountDto);
+       return new ResponseEntity<>(addedAccount, HttpStatus.CREATED);
+    }
+
+    // Get Account REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
+       AccountDto account = accountService.getAccountById(id);
+        return ResponseEntity.ok(account);
+    }
+
+    // Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+       Double amount = request.get("amount");
+       AccountDto accountDto = accountService.deposit(id, amount);
+       return ResponseEntity.ok(accountDto);
     }
 }
--- src/main/java/com/harshraj9988/payment_simulator/service/AccountService.java
+++ src/main/java/com/harshraj9988/payment_simulator/service/AccountService.java
@@ -5,4 +5,8 @@ import com.harshraj9988.payment_simulator.dto.AccountDto;
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, Double amount);
 }

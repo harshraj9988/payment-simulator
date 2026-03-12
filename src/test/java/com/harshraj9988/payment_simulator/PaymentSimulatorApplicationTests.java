25efa0267674fa8e82084d4dd497fa275178d132
Harsh Raj <harshraj9988@gmail.com>
2026-03-12 14:44:51 +0530

    initial commit

---
+++ src/test/java/com/harshraj9988/payment_simulator/PaymentSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshraj9988.payment_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
